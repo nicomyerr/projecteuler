4130a6eb07ca25e85cc2795e319a428f45a867f1
nicomyerr <mail@nicomeyer.me>
2025-01-10 19:28:35 +0100

    feat(java): solved problem 7

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ solutions for project-euler problems
 
 | language | problems solved |
 | -------- | --------------- |
-| java     | 1-6             |
+| java     | 1-7             |
 | rust     | 1,2             |
---
+++ java/p7.java
@@ -0,0 +1,26 @@
+public class p7 {
+  public static void main(String[] args) {
+    System.out.println("Solution of problem 7: " + solve());
+  }
+
+  private static int solve() {
+    int number = 2;
+    int counter = 1;
+    while (counter <= 10001) {
+      if (isPrime(number)) {
+        counter++;
+      }
+      number++;
+    }
+    return number - 1;
+  }
+
+  private static boolean isPrime(final int number) {
+    for (int i = 1; i <= number; i++) {
+      if (number % i == 0 && i != 1 && i != number) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
